0d899523a414f714daefa5e43ad78a3daea55ec7
allelimo <allelimo@gmail.com>
2016-10-29 22:48:18 +0200

    calcolo ipt per prov e calcolo vari fissi

--- delegazione/my-app.js
+++ delegazione/my-app.js
@@ -259,7 +259,7 @@ function fDisabilitaInput(nomelista) {
 // pagna generic - futura pdp
 // prendi i valori da impostazioni on init
 // button -> predni i valori della pagina
-myApp.onPageInit("generic", function (page) {
+myApp.onPageInit("temp01", function (page) {
     
      // da pagina 
     var storedData = myApp.formGetData('form_impostazioni_4');  // il form della seconda pagina form_gen2
@@ -272,8 +272,44 @@ myApp.onPageInit("generic", function (page) {
         alert(dirittiPosta);
     }
 
+     // da pagina impostazioni 
+    var storedData1 = myApp.formGetData('form_impostazioni_1');  // il form della seconda pagina form_gen2
+    if(storedData1) {
+        //alert("preso2");
+        var cippa1 = JSON.stringify(storedData1); // qui ci sono tutte le coppie nome/valore
+        //myApp.alert(cippa);
+        var lippa1 = JSON.parse(cippa1);
+        var dirittiPra = parseFloat(lippa1.pradir); 
+        var bolloPra = parseFloat(lippa1.praimp); 
+        //var uella = lippa.checkcdp;
+        //alert(dirittiPra + bolloPra);
+        document.getElementById("opzione7").value = (dirittiPra + bolloPra);
+    }
+
+    var storedData2 = myApp.formGetData('form_impostazioni_2');  // il form della seconda pagina form_gen2
+    if(storedData2) {
+        var cippa2 = JSON.stringify(storedData2); // qui ci sono tutte le coppie nome/valore
+        var lippa2 = JSON.parse(cippa2);
+        var dirittiUmc = parseFloat(lippa2.umcdir); 
+        var bolloUmc = parseFloat(lippa2.umcimp); 
+        document.getElementById("opzione8").value = (dirittiUmc + bolloUmc + dirittiPosta + dirittiPosta);
+    }
+
+    var storedData3 = myApp.formGetData('form_impostazioni_3');  // il form della seconda pagina form_gen2
+    if(storedData3) {
+        var cippa3 = JSON.stringify(storedData3); // qui ci sono tutte le coppie nome/valore
+        var lippa3 = JSON.parse(cippa3);
+        var dirittiAci1 = parseFloat(lippa3.acidir1); 
+        var dirittiAci2 = parseFloat(lippa3.acidir2); 
+        var dirittiAci3 = parseFloat(lippa3.acidir3); 
+
+        document.getElementById("opzione9").value = (dirittiAci1 + dirittiAci2 + dirittiAci3);
+        document.getElementById("opzione10").value = ((dirittiAci1 + dirittiAci2 + dirittiAci3) * 0.22);
+
+    }
     
-    
+
+
     // 1
 });
 
@@ -437,7 +473,7 @@ function fCheckMyClick2() {
 
 // usata in pagina generic come test
 function qualcosa(){
-    alert("qualcosa");
+    //alert("qualcosa");
 
 // var iptbase = 150.81;
 // var iptcoeff = 3.51;
@@ -481,14 +517,20 @@ var test = $$("select#provincia").val();
          alert(test);
     }
 
-var province10 = ["mi", "bo", "to", "ve"];
-var province20 = ["mb", "fe", "rm", "vi"];
+var province00 = ["ao","bz","tn"];
+var province10 = ["mt"];
+var province20 = ["ar","av","bn","ci","gr","lt","pn","re","sr","ts","ud","vi"];
+var province25 = ["fe","kr","so"];
 var provinciaselezionata = null;
 
-if (province10.indexOf(test) > -1 ) {
+if (province00.indexOf(test) > -1 ) {
+    provinciaselezionata = 1.0;
+} else if (province10.indexOf(test) > -1 ) {
     provinciaselezionata = 1.1;
 } else if (province20.indexOf(test) > -1 ) {
     provinciaselezionata = 1.2;
+} else if (province25.indexOf(test) > -1 ) {
+    provinciaselezionata = 1.25;
 } else {
     provinciaselezionata = 1.3;
 }
